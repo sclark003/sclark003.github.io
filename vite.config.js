7d071e59aa3b8321ed177be491df7fc4c6904dff
sclark003 <sarah@foxeshollow.com>
2026-05-10 21:24:43 +0100

    Redeploy

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/sclark003.github.io/',
   server: {
     port: 1234,
-    base: '/sclark003.github.io/',
   },
 });
